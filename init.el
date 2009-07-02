0103b7645d85d253c852f5e8a90e0cb3bff3ed0c
invertedplate <invertedplate@gmail.com>
2009-07-02 17:08:51 -0500

    AS3 y algo de las mumamos...

--- init.el
+++ init.el
@@ -70,6 +70,8 @@
 (load-file "~/wallemacs/site-lisp/color-theme-wunki.el")
 (load-file "~/wallemacs/site-lisp/color-theme-nightingale.el")
 (load-file "~/wallemacs/site-lisp/color-theme-blue.el")
+(load-file "~/wallemacs/site-lisp/color-theme-github.el")
+(load-file "~/wallemacs/site-lisp/color-theme-hober2.el")
 ;; (load-file "~/wallemacs/site-lisp/color-theme-zenburn.el")
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Soporte para el GIT y SVN;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -222,3 +224,93 @@
 ;; poniendo cosas para flex
 (load-file "~/wallemacs/flex-dev-mode/fcsh-compile.el")
 (load-file "~/wallemacs/flex-dev-mode/fcsh-mode.el")
+
+;; no lo carga, luego checo
+;; (load-file "~/wallemacs/site-lisp/actionscript-mode.el")
+
+
+;; a ver que pasa
+
+;; (require 'actionscript)
+;; esto lo encontre aca...http://github.com/zpinter/emacs.d/blob/3adb41b6f864b50a88698a9083eb59fe3f0bd129/290-flex/init.el
+
+;; (defconst mumamo-actionscript-tag-start-regex
+;;   (rx "<mx:Script>"
+;;       ;; FIX-ME: Commented out because of bug in Emacs
+;;       ;;
+;;       ;;(optional (0+ space) "<![CDATA[" )
+;;       ))
+ 
+;; (defun mumamo-search-bw-exc-start-inlined-actionscript (pos min)
+;;   "Helper for `mumamo-chunk-inlined-actionscript'.
+;; POS is where to start search and MIN is where to stop."
+;;   (goto-char (+ pos 10))
+;;   (let ((marker-start (search-backward "<mx:Script" min t))
+;;         exc-mode
+;;         exc-start)
+;;     (when marker-start
+;;       (when (looking-at mumamo-actionscript-tag-start-regex)
+;;         (setq exc-start (match-end 0))
+;;         (goto-char exc-start)
+;;         (when (<= exc-start pos)
+;;           ;;(cons (point) 'javascript-mode)
+;;           (list (point) 'actionscript-mode '(nxml-mode))
+;;           )
+;;         ))))
+ 
+;; (defun mumamo-search-bw-exc-end-inlined-actionscript (pos min)
+;;   "Helper for `mumamo-chunk-inlined-actionscript'.
+;; POS is where to start search and MIN is where to stop."
+;;   (mumamo-chunk-end-bw-str pos min "</mx:Script>"))
+ 
+;; (defun mumamo-search-fw-exc-start-inlined-actionscript (pos max)
+;;   "Helper for `mumamo-chunk-inlined-actionscript'.
+;; POS is where to start search and MAX is where to stop."
+;;   (goto-char (1+ pos))
+;;   (skip-chars-backward "^<")
+;;   ;; Handle <![CDATA[
+;;   (when (and
+;;          (eq ?< (char-before))
+;;          (eq ?! (char-after))
+;;          (not (bobp)))
+;;     (backward-char)
+;;     (skip-chars-backward "^<"))
+;;   (unless (bobp)
+;;     (backward-char 1))
+;;   (let ((exc-start (search-forward "<mx:Script" max t))
+;;         exc-mode)
+;;     (when exc-start
+;;       (goto-char (- exc-start 10))
+;;       (when (looking-at mumamo-actionscript-tag-start-regex)
+;;         (goto-char (match-end 0))
+;;         (point)
+;;         ))))
+ 
+;; (defun mumamo-search-fw-exc-end-inlined-actionscript (pos max)
+;;   "Helper for `mumamo-chunk-inlined-actionscript'.
+;; POS is where to start search and MAX is where to stop."
+;;   (save-match-data
+;;     (mumamo-chunk-end-fw-str pos max "</mx:Script>")))
+ 
+;; (defun mumamo-chunk-inlined-actionscript (pos min max)
+;;   "Find <script>...</script>.  Return range and 'javascript-mode.
+;; See `mumamo-find-possible-chunk' for POS, MIN and MAX."
+;;   (mumamo-find-possible-chunk pos min max
+;;                               'mumamo-search-bw-exc-start-inlined-actionscript
+;;                               'mumamo-search-bw-exc-end-inlined-actionscript
+;;                               'mumamo-search-fw-exc-start-inlined-actionscript
+;;                               'mumamo-search-fw-exc-end-inlined-actionscript))
+ 
+ 
+;; (define-mumamo-multi-major-mode mxml-actionscript-mumamo-mode
+;;     "Turn on multiple major modes for MXML with main mode `nxml-mode'.
+;; This covers inlined style and script for mxml."
+;;   ("NXML Family" nxml-mode
+;;                  (
+;;                   mumamo-chunk-inlined-actionscript
+;;                   )))
+ 
+;; (add-to-list 'auto-mode-alist '("\\.mxml\\'" . mxml-actionscript-mumamo-mode))
+
+;; muchas cosas interesantes por aca http://github.com/zpinter/emacs.d/tree/master
+
