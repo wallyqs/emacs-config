25408f2c8df0fed597a82d7040b9349ca118e4cf
invertedplate <invertedplate@gmail.com>
2009-10-09 11:38:41 -0500

    updated Cedet

--- cedet-1.0beta3b/contrib/contrib-loaddefs.el
+++
@@ -1,21 +0,0 @@
-;;; contrib-loaddefs.el --- Auto-generated CEDET autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (wisent-csharp-default-setup) "wisent-csharp" "wisent-csharp.el"
-;;;;;;  (16486 46885))
-;;; Generated autoloads from wisent-csharp.el
-
-(autoload (quote wisent-csharp-default-setup) "wisent-csharp" nil nil nil)
-
-(add-hook (quote csharp-mode-hook) (function wisent-csharp-default-setup))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; contrib-loaddefs.el ends here
