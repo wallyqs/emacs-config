8490b6a7c1b12f3ea66daaf0543f0b4ceec2f58f
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2010-12-24 00:10:27 +0900

    Added fix for js2-mode

--- config/wallemacs-defaults.el
+++ config/wallemacs-defaults.el
@@ -198,8 +198,8 @@
 (require 'wallemacs-javascript)
 
 (set-frame-parameter (selected-frame) 'alpha 98)
-(scroll-bar-mode nil)
-(tool-bar-mode nil)
+;; (scroll-bar-mode)
+;; (tool-bar-mode)
 (setq scroll-margin 0 scroll-conservatively 100000 scroll-preserve-screen-position 1)
 (display-time)
 (show-paren-mode t)
@@ -213,5 +213,75 @@
 ;; Objc-mode as major mode for Objective j
 ;; (add-to-list 'auto-mode-alist '("\\.j$" . objc-mode))
 (require 'objj-mode)
+
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; set up path on OS x
+(when (equal system-type 'darwin)
+  (setenv "PATH" (concat "/opt/local/bin:/usr/local/bin:" (getenv "PATH")))
+  (push "/opt/local/bin" exec-path))
+
+;; fixing the js2-mode, taken from https://github.com/mitchellh/dotfiles/blob/master/emacs.d/modes.el
+(require 'espresso)
+
+;; js-mode (espresso)
+;; Espresso mode has sane indenting so we use that.
+(setq js-indent-level 2)
+
+;; Customize JS2
+(setq js2-basic-offset 2)
+(setq js2-cleanup-whitespace t)
+
+;; Custom indentation function since JS2 indenting is terrible.
+;; Uses js-mode's (espresso-mode) indentation semantics.
+;;
+;; Based on: http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
+;; (Thanks!)
+(defun my-js2-indent-function ()
+  (interactive)
+  (save-restriction
+    (widen)
+    (let* ((inhibit-point-motion-hooks t)
+           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
+           (offset (- (current-column) (current-indentation)))
+           (indentation (js--proper-indentation parse-status))
+           node)
+
+      (save-excursion
+
+        ;; I like to indent case and labels to half of the tab width
+        (back-to-indentation)
+        (if (looking-at "case\\s-")
+            (setq indentation (+ indentation (/ js-indent-level 2))))
+
+        ;; consecutive declarations in a var statement are nice if
+        ;; properly aligned, i.e:
+        ;;
+        ;; var foo = "bar",
+        ;;     bar = "foo";
+        (setq node (js2-node-at-point))
+        (when (and node
+                   (= js2-NAME (js2-node-type node))
+                   (= js2-VAR (js2-node-type (js2-node-parent node))))
+          (setq indentation (+ 4 indentation))))
+
+      (indent-line-to indentation)
+      (when (> offset 0) (forward-char offset)))))
+
+(defun my-js2-mode-hook ()
+  (if (not (boundp 'js--proper-indentation))
+      (progn (js-mode)
+             (remove-hook 'js2-mode-hook 'my-js2-mode-hook)
+             (js2-mode)
+             (add-hook 'js2-mode-hook 'my-js2-mode-hook)))
+  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
+  (define-key js2-mode-map [(return)] 'newline-and-indent)
+  (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
+  (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
+  (message "JS2 mode hook ran."))
+
+;; Add the hook so this is all loaded when JS2-mode is loaded
+(add-hook 'js2-mode-hook 'my-js2-mode-hook)
 
 (provide 'wallemacs-defaults)
